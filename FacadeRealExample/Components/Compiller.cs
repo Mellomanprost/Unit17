4c7fe5371a7c970c17d973a0f48ff00245ed43f4
Mellomanprost <anoppa@mail.ru>
2022-12-14 11:53:25 +0300

    Added Facade pattern example

---
+++ FacadeRealExample/Components/Compiller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeRealExample.Components
+{
+    /// <summary>
+    /// Класс, вызывающий компилятор
+    /// </summary>
+    class Compiller
+    {
+        public void Compile()
+        {
+            Console.WriteLine("Компиляция приложения");
+        }
+    }
+}
