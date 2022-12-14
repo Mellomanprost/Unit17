52f461dc1abd15383b4412f3bd4ac2343a2a1cc4
Mellomanprost <anoppa@mail.ru>
2022-12-14 11:30:19 +0300

    Added Composite pattern example

---
+++ CompositeExample/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CompositeExample.Components;
+
+namespace CompositeExample
+{
+    /// <summary>
+    /// Клиентский код
+    /// </summary>
+    class Client
+    {
+        public static void Main()
+        {
+            // Создание корневой папки
+            Component rootFolder = new Folder("Root");
+
+            //Создание файла - компонента низшего уровня
+            Component myFile = new File("MyFile.txt");
+
+            //Создание папки с документами
+            Folder documentsFolder = new Folder("MyDocuments");
+
+            //Добавляем файл в корневую папку
+            rootFolder.Add(myFile);
+
+            //Добавляем подпапку для документов в корневую папку
+            rootFolder.Add(documentsFolder);
+
+            //Показываем контент первой папки
+            rootFolder.Display();
+        }
+    }
+}
